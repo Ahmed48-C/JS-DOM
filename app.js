da2ab7426920eba5e75fce88fe8bc4ba320e175e
Ahmed48-C <ahmed48coder@gmail.com>
2022-03-02 08:14:26 +0800

    changing text & html content

--- app.js
+++ app.js
@@ -18,5 +18,9 @@ var books = document.querySelectorAll('#book-list li .name');
 // console.log(books);
 
 Array.from(books).forEach(function(book){
-    console.log(book);
+    book.textContent += '(book title)';
   });
+
+const bookList = document.querySelector('#book-list');
+bookList.innerHTML = '<h2>Books and more books...';
+bookList.innerHTML += '<p>This is how you add HTML</p>';
